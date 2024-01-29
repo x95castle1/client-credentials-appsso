f7d32da4c47f3c60576b94e02be9f0efda5fe4a1
Jeremy Castle <x95castle1@yahoo.com>
2024-01-29 13:37:56 -0600

    Initial Commit

---
+++ src/main/java/io/pivotal/identityService/samples/clientcredentials/app/TodoRequest.java
@@ -0,0 +1,13 @@
+package io.pivotal.identityService.samples.clientcredentials.app;
+
+public class TodoRequest {
+    private String task;
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+}

bcb709c12009f004411619dbe5bf76c6c612c307
Jeremy Castle <x95castle1@yahoo.com>
2024-01-30 09:47:40 -0600

    Updated Spring Security to not use Okta JDK

--- src/main/java/com/auth0/example/security/SecurityConfig.java
+++ src/main/java/com/auth0/example/security/SecurityConfig.java
@@ -4,7 +4,6 @@ package com.auth0.example.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
